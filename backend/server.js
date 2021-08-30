c0d05a90ca1729abe0ef74c65d4ccaa488dc82a8
Shivanshu Saxena <shivanshudev02@gmail.com>
2021-08-31 03:42:04 +0530

    ready to run

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 const connectDb = require("./configDB/db.js");
 const userRouter = require("./routes/userRoutes.js");
@@ -8,7 +9,12 @@ const app = express();
 connectDb();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/../frontend/build")));
 
 app.use("/api", userRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../frontend/build", "index.html"));
+});
 
 app.listen(PORT || 5000);
